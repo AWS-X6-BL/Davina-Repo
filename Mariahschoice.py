6fb087a30936e6adbf109f89cecea212acd5c975
DavI-Gen <DavinaI@genstudents.org>
2025-05-27 14:03:06 +0100

    Add files via upload
    Some of my first Python scripts.

---
+++ Mariahschoice.py
@@ -0,0 +1,166 @@
+# foods = ["tuna", "salmon", "mackerel", "trout"]
+
+# print("Okay Weasley, you can eat any of the following for dinner:")
+
+# for food in foods:
+#     print("You can have " + food + " for dinner. ")
+#     print("or....")
+   
+# print("Nothing!")
+
+# favorite_foods = ['tuna', 'salmon', 'sardines', 'mackerel']
+# print(favorite_foods[0])
+
+# for food in favorite_foods:
+#     print("You can have " + food + " for dinner. ")
+#     print("Or a head scritch if you are not hungry")
+
+# favorite_foods = ['tuna', 'salmon', 'sardines', 'mackerel']
+
+# for food in favorite_foods:
+#     print(food)
+
+
+# dogs = ['frankie', 'otto', 'daisy', 'flossie']
+
+# for dog in dogs:
+#     print(dog)
+
+# foods = ["tuna", "salmon", "mackerel", "trout"]
+
+# print("Okay Weasley, you can eat any of the following for dinner:")
+
+# for food in foods:
+#     print("You can have " + food + " for dinner. ")
+#     print("or....")
+   
+# print("Nothing!")
+
+# songs = ["a hit summer song", "a catchy xmas tune", "a Nu metal banger" "a mildy obscure gospel album"]
+
+# print("Okay Mariah Carey you may have one of the following:")
+
+# for song in songs:
+#     print("you can have " + song + " and have a lasting music career.")
+#     print("or...")
+# print("a life where you never perform music ever again")
+
+
+
+# numbers = [1, 2, 3, 4, 5]
+
+# for number in numbers:
+#     print(number)
+
+
+# for value in range(1,6):
+#     print(value)
+
+
+# for value in range(0,21,3):
+#     print(value)
+
+# my_list = list('hello noche you sweet kitty')
+
+# print(my_list)
+
+
+# numbers = list(range(1,6))
+# print(numbers)
+
+
+# digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+# print(min(digits))
+# print(max(digits))
+# print(sum(digits))
+
+
+
+# numbers = list(range(2,9))
+# print(min(numbers))
+# print(max(numbers))
+# print(sum(numbers))
+
+# foods = ["tuna", "salmon", "mackerel", "trout"]
+
+# print(foods[2:])
+
+
+# foods = ["tuna", "salmon", "mackerel", "trout"]
+
+# print("Alright Weasley and Noche, this is what we can have for dinner: ")
+# for food in foods[0:4]:
+#     print(food.title())
+
+
+
+# plants = ['hawthoria', 'palm', 'pothos', 'barrel cactus', 'prayer plant']
+# print("Here are my favorite plants out of my list:")
+# for plant in plants[0:4]:
+#     print(plant.title())
+
+
+weasley_foods = ["tuna", "salmon", "mackerel", "trout"]
+noche_foods = weasley_foods[:]
+
+# weasley_foods.append('chicken')
+# noche_foods.append('cheese')
+
+# print("The foods Weasley likes to eat is: ")
+# print(weasley_foods)
+
+# print("The foods Noche likes to eat is: ")
+# print(noche_foods)
+
+
+
+# #a simple tuple
+# foods = ("tuna", "salmon", "mackerel", "trout")
+# print(foods)
+
+
+# #a simple tuple; accessing element
+# foods = ("tuna", "salmon", "mackerel", "trout")
+# print(foods)
+# print(foods[2])
+
+# #defining a simple tuple
+# plants = ('cacti', 'pothos', 'eucalyptus')
+# print(plants)
+
+
+# #accessing an element in a tuple is the same as accessing one in a list
+# plants = ('cacti', 'pothos', 'eucalyptus')
+# print(plants[0])
+
+
+# plants = ('cacti', 'pothos', 'eucalyptus')
+# for plant in plants:
+#     print(plant)
+
+# plants = ('palm', 'tree', 'lily pad')
+# for plant in plants:
+#     print(plant)
+   
+# print(plants)
+
+# foods = ["tuna", "salmon", "mackerel", "trout"]
+
+# print("Okay Weasley, you can eat any of the following for dinner:")
+
+# for food in foods:
+#     print("You can have " + food + " for dinner. ")
+#     print("or....")
+   
+# print("Nothing!")
+
+
+songs = ["a hit summer song", "a catchy xmas tune", "a Nu metal banger" "a mildy obscure gospel album"]
+
+print("Okay Mariah Carey you may have one of the following:")
+
+for song in songs:
+    print("you may have " + song + " and have a renouned and lasting music career.")
+    print("or...")
+print("A life where you can never perform any music ever again")
+print("Choose wisely!")
---
+++ PirateGary.py
@@ -0,0 +1,34 @@
+#1 Store a message in a variable, then print that message.
+
+name = "gary"
+print (message)
+
+#2 Store a message in a variable, and print that message. Then change the value of your variable to a new message, and print the new message.
+
+message = "Ahoy thar matey. A pirates life for me."
+
+#3 Store a person’s name in a variable, and print a message to that person
+print ("Ahoy Thar " +name.title())
+
+#4 Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+print (""+name.lower())
+print (""+name.upper())
+print (""+name.title())
+
+#5 Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+operation = 6 + 2
+print(operation)
+
+operation = 16-8
+print (operation)
+
+operation = 2 * 4
+print (operation)
+
+operation = 72 / 9
+print (operation)
+
+# #6 Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message
+favnumber = 23 
+print("My favorite number is " +str(favnumber))
